51e03b692166d742bfb28d7a3feee8594f38df83
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2024-12-26 22:58:38 +0000

    forgot page done

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from '../Pages/LoginForm';
+import Forgot from '../Components/Forgot/Forgot';
 
 
 const Routers = () => {
@@ -8,6 +9,7 @@ const Routers = () => {
     <Router>
       <Routes>
         <Route path="/login-form" element={<LoginForm />} />
+        <Route path="/forgot-password" element={<Forgot />} />
       </Routes>
     </Router>
   );
